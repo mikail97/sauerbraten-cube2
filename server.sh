8d62029cc45b3f3ff79ef39b548f94e181050a82
Mikail <jkrdj2013@gmail.com>
2016-11-06 20:44:13 +0100

    no message

--- server.sh
+++ server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-cd usr/share/games/sauerbraten/data
-rm menus.cfg
-cd /home/$USERNAME/sauerbraten-cube2
-mv menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
-cd /home/$USERNAME/sauerbraten-cube2
-mv server-init.cfg /home/$USERNAME/.sauerbraten/server-init.cfg
+dpkg --add-architecture i386
+apt-get update
+apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+rm /usr/share/games/sauerbraten/data/menus.cfg
+mv /home/$USERNAME/sauerbraten-cube2/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
+mv /home/$USERNAME/sauerbraten-cube2/server-init.cfg /home/$USERNAME/.sauerbraten/server-init.cfg
 exit
