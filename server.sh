9a9fb884c63440515323e84122103257f102bc51
Mikail <jkrdj2013@gmail.com>
2016-11-06 20:30:38 +0100

    server v 0.1

--- server.sh
+++ server.sh
@@ -2,5 +2,5 @@
 # -*- ENCODING: UTF-8 -*-
 rm /usr/share/games/sauerbraten/data/menus.cfg
 mv /home/$USERNAME/sauerbraten-cube2/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
-
+mv /home/$USERNAME/sauerbraten-cube2/server-init.cfg /home/$USERNAME/.sauerbraten/server-init.cfg
 exit
