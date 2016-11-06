bdc93d5040e56523ca858643453cb0285ec10f48
Mikail <jkrdj2013@gmail.com>
2016-11-06 20:58:51 +0100

    no message

--- server.sh
+++ server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-dpkg --add-architecture i386
-apt-get update
-apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
 rm /usr/share/games/sauerbraten/data/menus.cfg
-mv /home/$USERNAME/sauerbraten-cube2/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
-mv /home/$USERNAME/sauerbraten-cube2/server-init.cfg /home/$USERNAME/.sauerbraten/server-init.cfg
+mv /home/$USER/sauerbraten-cube2/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
+mv /home/$USER/sauerbraten-cube2/server-init.cfg /home/$USER/.sauerbraten/server-init.cfg
 exit
