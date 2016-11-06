9bc0aef043d7405057b3e6e97c2ab102772500cb
Mikail <jkrdj2013@gmail.com>
2016-11-06 20:19:21 +0100

    script para cambiar el idioma de varios menus V 0.1

---
+++ idioma.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+rm /usr/share/games/sauerbraten/data/menus.cfg
+mv /home/$USERNAME/sauerbraten-cube2/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
+exit
