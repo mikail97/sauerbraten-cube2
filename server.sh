ed17e1e944fb4f3c021bb3a12c77f8a6c967d5d8
Mikail <jkrdj2013@gmail.com>
2016-11-06 22:59:24 +0100

    no message

--- server.sh
+++ server.sh
@@ -3,19 +3,19 @@
 
 #BORRAMOS TODOS LOS FICHEROS EXISTENTES QUE VAYAMOS A REEMPLAZAR EN DATA
 rm /usr/share/games/sauerbraten/data/menus.cfg
-rm /home/$USER/sauerbraten-cube2/data/background.png
-rm /home/$USER/sauerbraten-cube2/data/background_decal.png
-rm /home/$USER/sauerbraten-cube2/data/background_detail.png
-rm /home/$USER/sauerbraten-cube2/data/cube.png
-rm /home/$USER/sauerbraten-cube2/data/cube2badge.png
-rm /home/$USER/sauerbraten-cube2/data/guioverlay.png
-rm /home/$USER/sauerbraten-cube2/data/guiskin.png
-rm /home/$USER/sauerbraten-cube2/data/guislider.png
-rm /home/$USER/sauerbraten-cube2/data/loading_bar.png
-rm /home/$USER/sauerbraten-cube2/data/loading_frame.png
-rm /home/$USER/sauerbraten-cube2/data/logo.png
-rm /home/$USER/sauerbraten-cube2/data/logo_1024.png
-rm /home/$USER/sauerbraten-cube2/data/mapshot_frame.png
+rm /usr/share/games/sauerbraten/data/background.pn
+rm /usr/share/games/sauerbraten/data/background_decal.png
+rm /usr/share/games/sauerbraten/data/background_detail.png
+rm /usr/share/games/sauerbraten/data/cube.png
+rm /usr/share/games/sauerbraten/data/cube2badge.png
+rm /usr/share/games/sauerbraten/data/guioverlay.png
+rm /usr/share/games/sauerbraten/data/guiskin.png
+rm /usr/share/games/sauerbraten/data/guislider.png
+rm /usr/share/games/sauerbraten/data/loading_bar.png
+rm /usr/share/games/sauerbraten/data/loading_frame.png
+rm /usr/share/games/sauerbraten/data/logo.png
+rm /usr/share/games/sauerbraten/data/logo_1024.png
+rm /usr/share/games/sauerbraten/data/mapshot_frame.png
 
 #MOVEMOS LOS FICHEROS A DATA
 mv /home/$USER/sauerbraten-cube2/data/menus.cfg /usr/share/games/sauerbraten/data/menus.cfg
